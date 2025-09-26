50dcb0757dc8ced667cb74c33e5e8cedeadd4ae8
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-09-26 20:18:13 +0530

    AREA OF TRIANGLE

---
+++ 01.C++basics/area_of_triangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int h,b;
+    float area;
+    cout<<"ENTER BREADTH "<<endl;
+    cin>>b;
+    cout<<"ENTER HEIGHT "<<endl;
+    cin>>h;
+    area = (float) (b*h)/2;
+    cout<<"AREA IS "<<area;
+    return 0;
+
+
+}
