6893b6eac341776e62a9978e56ae0ca87e60b67c
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-09-26 10:02:14 +0530

    add two numbers

---
+++ 01.C++basics/add_two_numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a,b;
+    cout<<"Enter 2 nos ";
+    cin>>a>>b;
+    int c = a+b;
+    cout<<"sum is "<<c<<endl;
+    return 0;
+    
+}
