5f975a4e6ff1097315a60e6a12324a89b6a20e76
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-09-27 06:06:16 +0530

    Area of a circle

---
+++ 01.C++basics/area_of_circle.cpp
@@ -0,0 +1,11 @@
+// to find area of a circle
+#include <iostream>
+using namespace std;
+int main(){
+    float r,area;
+    cout<<"Enter Radius ";
+    cin>>r;
+    area = 3.1425f*r*r;
+    cout<<"Area of a circle is "<<area<<endl;
+    return 0;
+}
