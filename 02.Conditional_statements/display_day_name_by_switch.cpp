e28a9778f8a96b3689687b3df98a3073a885c2c1
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-09-29 12:52:40 +0530

    add to display day name by switch file

---
+++ 02.Conditional_statements/display_day_name_by_switch.cpp
@@ -0,0 +1,47 @@
+// Displaying day name by switch
+#include <iostream>
+using namespace std;
+int main()
+{
+    int digitInput;
+
+    cout << "Enter the day number here: ";
+    cin >> digitInput;
+
+    switch (digitInput)
+    {
+
+    case 1:
+        cout << "Monday" << endl;
+        break;
+
+    case 2:
+        cout << "Tuesday" << endl;
+        break;
+
+    case 3:
+        cout << "Wednesday" << endl;
+        break;
+
+    case 4:
+        cout << "Thursday" << endl;
+        break;
+
+    case 5:
+        cout << "Friday" << endl;
+        break;
+
+    case 6:
+        cout << "Saturday" << endl;
+        break;
+
+    case 7:
+        cout << "Sunday" << endl;
+        break;
+
+    default:
+        cout << "Invalid Input !" << endl;
+        break;
+    }
+    return 0;
+}
