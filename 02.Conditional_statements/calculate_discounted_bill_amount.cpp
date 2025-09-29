e7e1dd6616d0903065e9c0dac975bb41a7b3b5be
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-09-29 09:09:51 +0530

    add to calculate discounted bill

---
+++ 02.Conditional_statements/calculate_discounted_bill_amount.cpp
@@ -0,0 +1,33 @@
+// Calculating the amount to pay after the discount
+#include <iostream>
+using namespace std;
+int main()
+{
+    int amount;
+    float discountedAmount; // Amount after the discount
+
+    cout << "Enter the amount you have to pay: ";
+    cin >> amount;
+
+    if (amount > 5000)
+    {
+        discountedAmount = (amount) - (20 * amount) / 100.0; // 20% discount
+        cout << "You have to pay: " << discountedAmount << endl;
+    }
+
+    else
+    {
+        if (amount >= 2000 && amount < 5000)
+        {
+            discountedAmount = (amount) - (10 * amount) / 100.0; // 10% discount
+            cout << "You have to pay: " << discountedAmount << endl;
+        }
+
+        else
+        {
+            discountedAmount = (amount) - (5 * amount) / 100.0; // 5% discount
+            cout << "You have to pay: " << discountedAmount << endl;
+        }
+    }
+    return 0;
+}
