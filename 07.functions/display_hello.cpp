967509f283a156ec1ef4449daabe1857f8bb27d3
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-06 06:37:20 +0530

    add display hello function file

---
+++ 07.functions/display_hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void display()
+{
+    cout << "Hello !" << endl;
+} // Function having name display is created
+
+int main()
+{
+    display(); // display function is called
+
+    return 0;
+}
