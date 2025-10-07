9ff7372b2d48d2730ac66bfcdfe3a25d36cf99f9
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-07 08:37:21 +0530

    add return by address file

---
+++ 07.functions/return_by_reference.cpp
@@ -0,0 +1,22 @@
+// Return By Reference
+#include <iostream>
+using namespace std;
+
+int &func(int &a)
+{
+
+    return a;
+}
+
+int main()
+{
+    int x = 23;
+
+    cout << func(x) << endl; // func(x) is indirectly becomes 'x'
+
+    func(x) = 39; // it means x=39
+
+    cout << func(x) << endl;
+
+    return 0;
+}
