687dd8f932a167e4a5497e31eac019485ec5f97b
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-07 07:47:02 +0530

    add pass by reference file

---
+++ 07.functions/pass_by_reference.cpp
@@ -0,0 +1,30 @@
+// Pass By Reference
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b)
+{
+
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter an integer: ";
+    cin >> num1;
+
+    cout << "Again! Enter an integer: ";
+    cin >> num2;
+
+    swap(num1, num2);
+
+    cout << "Original variables swapped: " << num1 << " " << num2 << endl; // Original variable also swapped
+
+    return 0;
+}
