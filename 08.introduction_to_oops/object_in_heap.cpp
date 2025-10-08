94132ca57952f05df1366b77fc1f2b43ab969c6f
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-08 16:59:29 +0530

    add object in heap file

---
+++ 08.introduction_to_oops/object_in_heap.cpp
@@ -0,0 +1,43 @@
+// Object in heap
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+public:
+    float length;
+    float breadth;
+
+    float area()
+    {
+        return length * breadth;
+    }
+
+    float perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+};
+
+int main()
+{
+    float length, breadth;
+
+    cout << "Enter your length of rectangle: ";
+    cin >> length;
+
+    cout << "Enter your breadth of rectangle: ";
+    cin >> breadth;
+
+    Rectangle *ptr;
+
+    ptr = new Rectangle;
+
+    ptr->length = length;
+    ptr->breadth = breadth;
+
+    cout << "Area is " << ptr->area() << endl;
+    cout << "Perimeter is " << ptr->perimeter() << endl;
+
+    return 0;
+}
