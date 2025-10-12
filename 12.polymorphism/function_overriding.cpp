e436287c5d05ecbadabfbd46aa7833eb7449bd56
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-13 04:00:07 +0530

    add function overriding file

---
+++ 12.polymorphism/function_overriding.cpp
@@ -0,0 +1,35 @@
+// Function Overriding
+#include <iostream>
+using namespace std;
+
+class Base
+{
+public:
+    void display()
+    {
+
+        cout << "Display of Base " << endl;
+    }
+};
+
+class Derived : public Base
+{
+public:
+    void display()
+    {
+
+        cout << "Display of Derived " << endl;
+    }
+};
+
+int main()
+{
+
+    Base b;
+    b.display();
+
+    Derived d;
+    d.display(); // function is redefined, sonit will access derived class
+
+    return 0;
+}
