40dbf0c263a221763dbd61cc93cf4e5561e37998
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-14 04:09:32 +0530

    add static members example 02 file

---
+++ 13.friend_static_member_inner_classes/static_members_example_02.cpp
@@ -0,0 +1,26 @@
+// Static Members Example 02
+#include <iostream>
+using namespace std;
+
+class Innova
+{
+public:
+    static int price;
+
+    static int getPrice()
+    {
+        return price;
+    }
+};
+int Innova ::price = 20;
+
+int main()
+{
+
+    Innova i1, i2;
+    cout << i1.price << endl;
+    cout << i2.price << endl;
+    cout << Innova::getPrice() << endl;
+
+    return 0;
+}
