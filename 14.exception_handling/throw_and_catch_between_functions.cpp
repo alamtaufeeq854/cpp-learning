653fbc455b3198b25b859cbde94eba40440b5bd2
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-14 17:01:47 +0530

    add throw and catch between functions file

---
+++ 14.exception_handling/throw_and_catch_between_functions.cpp
@@ -0,0 +1,36 @@
+// Throw and Catch Between Functions
+#include <iostream>
+using namespace std;
+
+division(int x, int y)
+{
+
+    if (y == 0)
+        throw 1;
+
+    return x / y;
+}
+
+int main()
+{
+    int num1, num2, result;
+
+    cout << "Enter the First number here: ";
+    cin >> num1;
+
+    cout << "Enter the Second number here: ";
+    cin >> num2;
+
+    try
+    {
+        result = division(num1, num2);
+        cout << "Division of numbers are " << result << endl;
+    }
+
+    catch (int e)
+    {
+        cout << "Division by zero is not possible " << endl;
+    }
+
+    return 0;
+}
