20c6b0c80456fa410ff2976c2447afa8cf3eded0
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-15 18:44:31 +0530

    add constant qualifiers file

---
+++ 16.constant_preprocessor_directives_namespaces/constant_qualifiers.cpp
@@ -0,0 +1,15 @@
+// Constant Qualifiers
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    const int x = 10;
+    int const y = 24;
+
+    //        x=55;   // Can't modify the 'x'
+    //        y=98;   // // Can't modify the 'y'
+
+    return 0;
+}
