1256331469dfe621bc963667ac8f687dd7b779a0
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-15 21:20:35 +0530

    add preprocessor directives in function file

---
+++ 16.constant_preprocessor_directives_namespaces/preprocessor_directives_in_function.cpp
@@ -0,0 +1,13 @@
+// Preprocessor Directives in Function
+#include <iostream>
+using namespace std;
+
+#define maxim(x, y) (x > y ? x : y)
+
+int main()
+{
+
+    cout << maxim(17, 23) << endl;
+
+    return 0;
+}
