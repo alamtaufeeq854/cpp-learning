2b1436c0ab8636d6622eecdfebb800d33308bc7a
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-17 09:22:15 +0530

    add final keyword file

---
+++ 20.c++11/final_keyword.cpp
@@ -0,0 +1,15 @@
+// Final Keyword
+#include <iostream>
+using namespace std;
+
+class parent final // final keyword restrict the inheritence
+{
+};
+
+class child : public parent
+{
+};
+
+int main()
+{
+}
