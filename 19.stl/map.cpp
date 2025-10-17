af603866527102d3973666c5578d89c7510c5a0f
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-17 07:39:04 +0530

    add map file

---
+++ 19.stl/map.cpp
@@ -0,0 +1,25 @@
+// STL by using map
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<int, string> m;
+    m.insert(pair<int, string>(1, "amar"));
+    m.insert(pair<int, string>(2, "akbar"));
+    m.insert(pair<int, string>(3, "anthany"));
+    map<int, string>::iterator itr;
+
+    for (itr = m.begin(); itr != m.end(); itr++)
+    {
+        cout << itr->first << " " << itr->second << endl;
+    }
+    map<int, string>::iterator itr1;
+    itr1 = m.find(2);
+
+    cout << "Value Found is" << endl;
+    cout << itr1->first << " " << itr1->second << endl;
+
+    return 0;
+}
