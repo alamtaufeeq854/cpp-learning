031dabc72efc559987e1013cfd421b098fddd750
Taufeeq Alam <alamtaufeeq854@gmail.com>
2025-10-18 15:38:23 +0530

    add reverse iterator file

---
+++ 06.strings/reverse_iterator.cpp
@@ -0,0 +1,16 @@
+//  Reverse Iterator
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str = "hello";
+
+    string::reverse_iterator it;
+
+    for (it = str.rbegin(); it != str.rend(); it++)
+    {
+        cout << *it;
+    }
+
+    return 0;
+}
